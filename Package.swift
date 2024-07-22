4638281a8db5667378ae7566e648b33fc4fc840c
hackiftekhar <hack.iftekhar@gmail.com>
2024-07-22 19:09:09 +0530

    1.0.0

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.6
+
+import PackageDescription
+
+let package = Package(
+    name: "IQKeyboardCore",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "IQKeyboardCore",
+            targets: ["IQKeyboardCore"]
+        )
+    ],
+    targets: [
+        .target(name: "IQKeyboardCore",
+            path: "IQKeyboardCore",
+            resources: [
+                .copy("Assets/PrivacyInfo.xcprivacy")
+            ],
+            linkerSettings: [
+                .linkedFramework("UIKit")
+            ]
+        )
+    ]
+)
